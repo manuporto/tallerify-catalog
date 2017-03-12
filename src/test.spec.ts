b8ee0a11988fba52007981ba57dfa38fe5821e32
Manuel Porto <manu.porto94@gmail.com>
2017-03-12 12:42:36 -0300

    Write tests in typscript, update dependencies.

---
+++ src/test.spec.ts
@@ -0,0 +1,10 @@
+import { expect } from 'chai';
+// if you used the '@types/mocha' method to install mocha type definitions, uncomment the following line
+import 'mocha';
+
+describe('Dummy test', () => {
+    it('1 equals 1', () => {
+    const result = 1;
+    expect(result).to.equal(1);
+});
+});
