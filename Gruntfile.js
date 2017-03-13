05a5d9e988703faa18598d6ed8327d5f03c43066
Manuel Porto <manu.porto94@gmail.com>
2017-03-12 22:06:35 -0300

    Change tresholds, and mocha_istanbul options

--- Gruntfile.js
+++ Gruntfile.js
@@ -85,10 +85,10 @@ module.exports = function(grunt) {
             check: {
                 options: {
                     thresholds: {
-                        branches: 100,
-                        functions: 100,
-                        lines: 100,
-                        statements: 100
+                        branches: 75,
+                        functions: 75,
+                        lines: 75,
+                        statements: 75
                     },
                     dir: "./coverage"
                 }
