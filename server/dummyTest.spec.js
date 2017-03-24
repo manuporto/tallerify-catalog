e0485727e0b95ac5cdba02b33c8ac07fd9cc9839
mporto <manu.porto94@gmail.com>
2017-03-24 13:04:19 -0300

    Check body parameters when POST /api/user

--- server/dummyTest.spec.js
+++
@@ -1,7 +0,0 @@
-describe("Dummy test spec file", () => {
-  describe("Dummy test", () => {
-    it("Assert 1 equals 1", () => {
-      expect(1).toEqual(1);
-    })
-  })
-})
--- server/routes/user.js
+++ server/routes/user.js
@@ -2,18 +2,38 @@ const winston = require('winston');
 var models = require('../models/index');
 
 getUsers = (req, res) => {
-  winston.log('info', `Get /users`);
+  winston.log('info', `GET /api/users`);
   models.User.findAll({}).then(users => {
-    winston.log('info', `Response: ${res}`);
     res.status(200).json(users);
   }).catch(reason => {
-    winston.log('warn', `Error when doing /users query: "${reason}"`);
+    winston.log('warn', `Error when doing GET /api/users: "${reason}"`);
     res.status(500);
   });
 };
 
+isReqBodyValid = (body) => {
+  return (
+    typeof body.userName !== 'undefined' &&
+    typeof body.firstName !== 'undefined' &&
+    typeof body.lastName !== 'undefined' &&
+    typeof body.country !== 'undefined' &&
+    typeof body.email !== 'undefined' &&
+    typeof body.birthdate !== 'undefined' &&
+    typeof body.images !== 'undefined'
+  );
+};
+
 postUser = (req, res) => {
-  winston.log('info', `Post /users with query ${JSON.stringify(req.body, null, 4)}`);
+  winston.log('info', `POST /api/users with body ${JSON.stringify(req.body, null, 4)}`);
+  if (!isReqBodyValid(req.body)) {
+    winston.log('warn', 'Request body parameters invalid');
+    res.status(400).json({
+      code: 0,
+      message: "string"
+    });
+    return;
+  }
+
   models.User.create({
     userName: req.body.userName,
     firstName: req.body.firstName,
@@ -25,6 +45,9 @@ postUser = (req, res) => {
   }).then(user => {
     winston.log('info', `Response: ${res}`);
     res.status(200).json(user);
+  }).catch(reason => {
+    winston.log('warn', `Error when doing POST /api/users: "${reason}"`);
+    res.status(500);
   });
 };
 
