662bc9d7c06ad7ce76359e707b46f22ace873d3d
mporto <manu.porto94@gmail.com>
2017-03-26 14:28:51 -0300

    Update validations

--- server/models/user.js
+++ server/models/user.js
@@ -2,13 +2,29 @@
 module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define('User', {
     href: DataTypes.STRING,
-    userName: { type: DataTypes.STRING, allowNull: false },
-    firstName: { type: DataTypes.STRING, allowNull: false },
-    lastName: { type: DataTypes.STRING, allowNull: false },
-    country: { type: DataTypes.STRING, allowNull: false },
-    email: { type: DataTypes.STRING, allowNull: false },
-    birthdate: { type: DataTypes.STRING, allowNull: false },
-    images: { type: DataTypes.ARRAY(DataTypes.STRING), allowNull: false },
+    userName: { type: DataTypes.STRING, allowNull: false, validate: {
+      notEmpty: true,
+    }},
+    firstName: { type: DataTypes.STRING, validate: {
+      notEmpty: true,
+    }},
+    lastName: { type: DataTypes.STRING, allowNull: false, validate: {
+      notEmpty: true,
+    }},
+    country: { type: DataTypes.STRING, allowNull: false, validate: {
+      notEmpty: true,
+    }},
+    email: { type: DataTypes.STRING, allowNull: false, validate: {
+      notEmpty: true,
+      isEmail: true
+    }},
+    birthdate: { type: DataTypes.STRING, allowNull: false, validate: {
+      notEmpty: true,
+      isDate: true
+    }},
+    images: { type: DataTypes.ARRAY(DataTypes.STRING), allowNull: false, validate: {
+      notEmpty: true,
+    }},
     contacts: { type: DataTypes.ARRAY(DataTypes.INTEGER), defaultValue: []}
   }, {
     classMethods: {
