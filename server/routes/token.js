2e8d40e05c5b843daa5c7353153e1b8dec8b1af7
Agustina Barbetta <agustina.barbetta@gmail.com>
2017-04-02 03:01:52 -0300

    Fix request body validator error message

--- server/routes/token.js
+++ server/routes/token.js
@@ -23,8 +23,8 @@ generateToken = (req, res) => {
   winston.log('info', `Validating request "${JSON.stringify(req.body, null, 4)}"`);
   jsonSchemaValidator.validate(req.body, expectedBodySchema, (error) => {
     if (error) {
-      winston.log('err', `Request body is invalid`);
-      return res.status(400).json({code: 400, message: `Invalid body: ${JSON.stringify(error.message, null, 4)}`});
+      winston.log('err', `Request body is invalid: ${error[0].message}`);
+      return res.status(400).json({code: 400, message: `Invalid body: ${error[0].message}`});
 
     } else {
       winston.log('info', `Querying database for user with credentials "${JSON.stringify(req.body, null, 4)}"`);
