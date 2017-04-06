942a05a5e76e74d32a14aa8a5b08f6eae0463a48
Manuel Porto <manu.porto94@gmail.com>
2017-04-06 00:11:32 -0300

    Add done function as parameter

--- server/test/routes/user.spec.js
+++ server/test/routes/user.spec.js
@@ -27,97 +27,98 @@ var expect = chai.expect;
 
 describe('User', () => {
 
-  it('should return version number', function(done) {
-    request(app)
-      .get('/api')
-      .end(function(err, res) {
-        console.log(res.statusCode);
-        // expect(res.body.version).to.be.ok;
-        expect(res.statusCode).to.equal(200);
-        done();
-      });
-  });
-
-  it('should return status code 200', () => {
-    request(app)
-      .get("/api/users")
-      .end(function(err,res){
-        console.log(res.statusCode);
-        // expect(res.body.version).to.be.ok;
-        expect(res.statusCode).to.equal('puto');
-        done();
-      });
-  });
-
-  // describe('/GET users', () => {
-  //   it('should return status code 200', () => {
-  //     request(app)
-  //     .get("/api/users")
+  // it('should return status code 200', done => {
+  //   request(app)
+  //     .get("/api/test1")
   //     .end(function(err,res){
   //       console.log(res.statusCode);
   //       expect(res.body.version).to.be.ok;
   //       expect(res.statusCode).to.equal('puto');
   //       done();
   //     });
-  //   });
+  // });
+  //
+  // it('should return version number', function(done) {
+  //   request(app)
+  //     .get('/api')
+  //     .end(function(err, res) {
+  //       console.log(res.statusCode);
+  //       expect(res.body.version).to.be.ok;
+  //       expect(res.statusCode).to.equal(200);
+  //       done();
+  //     });
   // });
 
-  // describe('/POST users', () => {
-  //   it('should return status code 400 when incorrect parameters are sent', () => {
-  //     chai.request(app)
-  //       .post('/api/users')
-  //       .send({
-  //         userName: 'jDoe',
-  //         firstName: 'John',
-  //         lastName: 'Doe'
-  //       }).end((err, res) => {
-  //       res.should.have.status(400);
-  //       done();
-  //     });
-  //   });
-  //
-  //   it('should return status code 200 when correct parameters are sent', () => {
-  //     chai.request(app)
-  //       .post('/api/users')
-  //       .send({
-  //         userName: 'postTest',
-  //         firstName: 'Post',
-  //         lastName: 'Test',
-  //         country: 'Mocha',
-  //         email: 'post@test.com',
-  //         birthdate: '1/1/1990',
-  //         images: []
-  //       }).end((err, res) => {
-  //       res.should.have.status(200);
-  //       done();
-  //     });
-  //   });
-  //
-  //   it('should return the expected body response when correct parameters are sent', () => {
-  //     const expected ={
-  //       id: 0,
-  //       userName: 'postTest',
-  //       firstName: 'Post',
-  //       lastName: 'Test',
-  //       country: 'Mocha',
-  //       email: 'post@test.com',
-  //       birthdate: '1/1/1990',
-  //       images: []
-  //     };
-  //     chai.request(app)
-  //       .post('/api/users')
-  //       .send({
-  //         userName: 'postTest',
-  //         firstName: 'Post',
-  //         lastName: 'Test',
-  //         country: 'Mocha',
-  //         email: 'post@test.com',
-  //         birthdate: '1/1/1990',
-  //         images: []
-  //       }).end((err, res) => {
-  //       res.body.should.equal(expected);
-  //       done();
-  //     });
-  //   });
-  // });
+  describe('/GET users', () => {
+    it('should return status code 200', done => {
+      request(app)
+      .get("/api/users")
+      .end((err,res) => {
+        expect(res.statusCode).to.equal(200);
+        done();
+      });
+    });
+  });
+
+  describe('/POST users', () => {
+    it('should return status code 400 when incorrect parameters are sent', done => {
+      request(app)
+        .post('/api/users')
+        .send({
+          userName: 'jDoe',
+          firstName: 'John',
+          lastName: 'Doe'
+        }).end((err, res) => {
+        expect(res.statusCode).to.equal(400);
+        done();
+      });
+    });
+
+    it('should return status code 200 when correct parameters are sent', done => {
+      request(app)
+        .post('/api/users')
+        .send({
+          userName: 'postTest',
+          password: '1234',
+          firstName: 'Post',
+          lastName: 'Test',
+          country: 'Mocha',
+          email: 'post@test.com',
+          birthdate: '1/1/1990',
+          images: ['']
+        }).end((err, res) => {
+        expect(res.statusCode).to.equal(200);
+        done();
+      });
+    });
+
+    it('should return the expected body response when correct parameters are sent', done => {
+      const expected ={
+        id: 0,
+        userName: 'postTest',
+        password: '1234',
+        firstName: 'Post',
+        lastName: 'Test',
+        country: 'Mocha',
+        email: 'post@test.com',
+        birthdate: '1/1/1990',
+        images: ['']
+      };
+      request(app)
+        .post('/api/users')
+        .send({
+          userName: 'postTest',
+          password: '1234',
+          firstName: 'Post',
+          lastName: 'Test',
+          country: 'Mocha',
+          email: 'post@test.com',
+          birthdate: '1/1/1990',
+          images: ['']
+        }).end((err, res) => {
+        expect(res.body).to.equal(expected);
+        done();
+      });
+    });
+  });
 })
