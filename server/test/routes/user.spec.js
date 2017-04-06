c70b7f0dfa0055804ccbd509d9122d1ac85a2797
Agustina Barbetta <agustina.barbetta@gmail.com>
2017-04-06 16:14:36 -0300

    User tests with constants + invalid parameters test

--- server/test/routes/user.spec.js
+++ server/test/routes/user.spec.js
@@ -71,12 +71,30 @@ describe('User', () => {
       request(app)
         .post('/api/users')
         .send({
-          userName: 'jDoe',
-          firstName: 'John',
-          lastName: 'Doe'
+          userName: constants.testUser.userName,
+          firstName: constants.testUser.firstName,
+          lastName: constants.testUser.lastName,
         }).end((err, res) => {
           res.should.have.status(400);
           done();
+      });
+    });
+
+    it('should return status code 400 when parameters invalid', done => {
+      request(app)
+        .post('/api/users')
+        .send({
+          userName: constants.invalidUser.userName,
+          password: constants.invalidUser.password,
+          firstName: constants.invalidUser.firstName,
+          lastName: constants.invalidUser.lastName,
+          country: constants.invalidUser.country,
+          email: constants.invalidUser.email,
+          birthdate: constants.invalidUser.birthdate,
+          images: constants.invalidUser.images
+        }).end((err, res) => {
+        res.should.have.status(400);
+        done();
       });
     });
 
@@ -84,14 +102,14 @@ describe('User', () => {
       request(app)
         .post('/api/users')
         .send({
-          userName: 'abrden',
-          password: '1234',
-          firstName: 'Agustina',
-          lastName: 'Barbetta',
-          country: 'Argentina',
-          email: 'a@a.com',
-          birthdate: '12/8/1994',
-          images: [ 'hello', 'world']
+          userName: constants.testUser.userName,
+          password: constants.testUser.password,
+          firstName: constants.testUser.firstName,
+          lastName: constants.testUser.lastName,
+          country: constants.testUser.country,
+          email: constants.testUser.email,
+          birthdate: constants.testUser.birthdate,
+          images: constants.testUser.images
         }).end((err, res) => {
           res.should.have.status(201);
           done();
@@ -102,25 +120,25 @@ describe('User', () => {
       request(app)
         .post('/api/users')
         .send({
-          userName: 'abrden',
-          password: '1234',
-          firstName: 'Agustina',
-          lastName: 'Barbetta',
-          country: 'Argentina',
-          email: 'a@a.com',
-          birthdate: '12/8/1994',
-          images: [ 'hello', 'world']
+          userName: constants.testUser.userName,
+          password: constants.testUser.password,
+          firstName: constants.testUser.firstName,
+          lastName: constants.testUser.lastName,
+          country: constants.testUser.country,
+          email: constants.testUser.email,
+          birthdate: constants.testUser.birthdate,
+          images: constants.testUser.images
         }).end((err, res) => {
           res.body.should.be.a('object');
           res.body.should.have.property('id');
-          res.body.should.have.property('userName').eql('abrden');
-          res.body.should.have.property('password').eql('1234');
-          res.body.should.have.property('firstName').eql('Agustina');
-          res.body.should.have.property('lastName').eql('Barbetta');
-          res.body.should.have.property('country').eql('Argentina');
-          res.body.should.have.property('email').eql('a@a.com');
-          res.body.should.have.property('birthdate').eql('12/8/1994');
-          res.body.should.have.property('images').eql([ 'hello', 'world']);
+          res.body.should.have.property('userName').eql(constants.testUser.userName);
+          res.body.should.have.property('password').eql(constants.testUser.password);
+          res.body.should.have.property('firstName').eql(constants.testUser.firstName);
+          res.body.should.have.property('lastName').eql(constants.testUser.lastName);
+          res.body.should.have.property('country').eql(constants.testUser.country);
+          res.body.should.have.property('email').eql(constants.testUser.email);
+          res.body.should.have.property('birthdate').eql(constants.testUser.birthdate);
+          res.body.should.have.property('images').eql(constants.testUser.images);
           res.body.should.have.property('contacts');
           res.body.should.have.property('href');
           done();
