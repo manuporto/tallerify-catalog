f7007ad98c39203dc1a4086e7146ac3c5d1a8227
Agustina Barbetta <agustina.barbetta@gmail.com>
2017-04-19 03:01:20 -0300

    Refactor token generation endpoints

--- src/routes/token.js
+++ src/routes/token.js
@@ -1,4 +1,5 @@
 const winston = require('winston');
+const promisify = require('promisify-node');
 const amanda = require('amanda');
 const models = require('../models/index');
 
@@ -18,93 +19,111 @@ const expectedBodySchema = {
   },
 };
 
+function validateRequestBody(body, callback) {
+  winston.log('info', `Validating request "${JSON.stringify(body, null, 4)}"`);
+  return jsonSchemaValidator.validate(body, expectedBodySchema, callback);
+}
+
+const validateJson = promisify(validateRequestBody);
+
+function invalidRequestBodyError(reasons, response) {
+  winston.log('warn', `Request body is invalid: ${reasons[0].message}`);
+  return response.status(400).json({ code: 400, message: `Invalid body: ${reasons[0].message}` });
+}
+
+function findWithUsernameAndPassword(model, username, password) {
+  winston.log('info', `Querying database for entry with username "${username}" and password "${password}"`);
+  return model.findAll({
+    where: {
+      userName: username,
+      password: password,
+    },
+  });
+}
+
+function resultIsValid(result, response) {
+  if (result.length === 0) {
+    winston.log('warn', 'No entry with such credentials');
+    response.status(500).json({ code: 500, message: 'No entry with such credentials' });
+    return false;
+  }
+  if (result.length > 1) {
+    winston.log('warn', `There is more than one entry with those credentials "${result}"`);
+    response.status(500).json({ code: 500, message: 'There is more than one entry with those credentials' });
+    return false;
+  }
+  return true;
+}
+
+function successfulTokenGeneration(result, response) {
+  winston.log('info', `Response: ${result}`);
+  response.status(201).json(result);
+}
+
+function successfulUserTokenGeneration(user, response) {
+  const result = Object.assign(
+    {},
+    {
+      token: user.id.toString(),
+      user: {
+        id: user.id,
+        href: user.href,
+        userName: user.userName,
+      },
+    });
+  successfulTokenGeneration(result, response);
+}
+
+function successfulAdminTokenGeneration(admin, response) {
+  const result = Object.assign(
+    {},
+    {
+      token: admin.id.toString(),
+      admin: {
+        id: admin.id,
+        userName: admin.userName,
+      },
+    });
+  successfulTokenGeneration(result, response);
+}
+
+function internalServerError(reason, response) {
+  winston.log('warn', `Unexpected error: ${reason}`);
+  return response.status(500).json({ code: 500, message: `Unexpected error: ${reason}` });
+}
+
 const generateToken = (req, res) => {
   winston.log('info', 'POST /api/tokens');
-
-  winston.log('info', `Validating request "${JSON.stringify(req.body, null, 4)}"`);
-  return jsonSchemaValidator.validate(req.body, expectedBodySchema, (error) => {
-    if (error) {
-      winston.log('warn', `Request body is invalid: ${error[0].message}`);
-      return res.status(400).json({ code: 400, message: `Invalid body: ${error[0].message}` });
-    }
-    winston.log('info', `Querying database for user with credentials "${JSON.stringify(req.body, null, 4)}"`);
-    return models.users.findAll({
-      where: {
-        userName: req.body.userName,
-        password: req.body.password,
-      },
-    }).then((users) => {
-      if (users.length === 0) {
-        winston.log('warn', 'No user with such credentials');
-        return res.status(500).json({ code: 500, message: 'No user with such credentials' });
-      }
-      if (users.length > 1) {
-        winston.log('warn', `There is more than one user with those credentials "${users}"`);
-        return res.status(500).json({ code: 500, message: 'There is more than one user with those credentials' });
-      }
-
-      const response = Object.assign(
-          {},
-        {
-          token: users[0].id.toString(),
-          user: {
-            id: users[0].id,
-            href: users[0].href,
-            userName: users[0].userName,
-          },
+  return validateJson(req.body)
+    .then(() => {
+      findWithUsernameAndPassword(models.users, req.body.userName, req.body.password)
+        .then((users) => {
+          if (!resultIsValid(users, res)) return;
+          successfulUserTokenGeneration(users[0], res);
+        }).catch((reason) => {
+          internalServerError(reason, res);
         });
-
-      winston.log('info', `Response: ${response}`);
-      return res.status(201).json(response);
-    }).catch((reason) => {
-      winston.log('warn', `Unexpected error: ${reason}`);
-      return res.status(500).json({ code: 500, message: `Unexpected error: ${reason}` });
+    })
+    .catch((error) => {
+      invalidRequestBodyError(error, res);
     });
-  });
 };
 
 const generateAdminToken = (req, res) => {
   winston.log('info', 'POST /api/tokens/admins');
-
-  winston.log('info', `Validating request "${JSON.stringify(req.body, null, 4)}"`);
-  return jsonSchemaValidator.validate(req.body, expectedBodySchema, (error) => {
-    if (error) {
-      winston.log('warn', `Request body is invalid: ${error[0].message}`);
-      return res.status(400).json({ code: 400, message: `Invalid body: ${error[0].message}` });
-    }
-    winston.log('info', `Querying database for admin with credentials "${JSON.stringify(req.body, null, 4)}"`);
-    return models.admins.findAll({
-      where: {
-        userName: req.body.userName,
-        password: req.body.password,
-      },
-    }).then((admins) => {
-      if (admins.length === 0) {
-        winston.log('warn', 'No admin with such credentials');
-        return res.status(500).json({ code: 500, message: 'No admin with such credentials' });
-      }
-      if (admins.length > 1) {
-        winston.log('warn', `There is more than one admin with those credentials "${admins}"`);
-        return res.status(500).json({ code: 500, message: 'There is more than one admin with those credentials' });
-      }
-
-      const response = Object.assign(
-        {},
-        {
-          token: admins[0].id.toString(),
-          admin: {
-            id: admins[0].id,
-            userName: admins[0].userName,
-          },
+  return validateJson(req.body)
+    .then(() => {
+      findWithUsernameAndPassword(models.admins, req.body.userName, req.body.password)
+        .then((admins) => {
+          if (!resultIsValid(admins, res)) return;
+          successfulAdminTokenGeneration(admins[0], res);
+        }).catch((reason) => {
+          internalServerError(reason, res);
         });
-
-      winston.log('info', `Response: ${response}`);
-      return res.status(201).json(response);
-    }).catch((reason) => {
-      winston.log('warn', `Unexpected error: ${reason}`);
-      return res.status(500).json({ code: 500, message: `Unexpected error: ${reason}` });
+    })
+    .catch((error) => {
+      invalidRequestBodyError(error, res);
     });
-  });
 };
 
 module.exports = { generateToken, generateAdminToken };
