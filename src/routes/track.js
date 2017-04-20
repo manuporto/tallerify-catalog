29e817742870cf6cbea79990f816cd99647cf9c8
Manuel Porto <manu.porto94@gmail.com>
2017-04-20 09:45:42 -0300

    Insert all artists into join table when creating new track

--- src/routes/track.js
+++ src/routes/track.js
@@ -8,16 +8,27 @@ function postTrack(req, res) {
 		name: req.body.name,
 		duration: req.body.duration
 	}).then(track => {
-		db(tables.artists).where({name: req.body.artists[0]}).select('id').then(id => {
-			logger.info(`Artist ID: ${JSON.stringify(id, null, 4)}`);
-			logger.info(`Track ID: ${JSON.stringify(track, null, 4)}`);
-			db(tables.artists_tracks).insert({
-				track_id: track[0].id,
-				artist_id: id[0].id
-			}).then(() => {
-				res.status(200).json(track);
-			})
-		});
+		let count = 0;
+		let currentArtist = 0;
+		const artistsLenght = req.body.artists.length;
+		while(count < artistsLenght) {
+			if (currentArtist < artistsLenght) {
+				db(tables.artists).
+					where({name: req.body.artists[currentArtist]}).
+					select('id').
+					then(id => {
+						logger.info(`Artist ID: ${JSON.stringify(id, null, 4)}`);
+						logger.info(`Track ID: ${JSON.stringify(track, null, 4)}`);
+						db(tables.artists_tracks).insert({
+							track_id: track[0].id,
+							artist_id: id[0].id
+						}).then(() => {
+							count++;
+						})
+				});
+			}
+		}
+		res.status(201).json(track);
 	})
 }
 
