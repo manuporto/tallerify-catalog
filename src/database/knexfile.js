12546e0d25f8803a66cc7fb705d13568200cfbc9
Agustina Barbetta <agustina.barbetta@gmail.com>
2017-04-21 02:31:00 -0300

    Update db uri for travis

--- src/database/knexfile.js
+++ src/database/knexfile.js
@@ -10,7 +10,7 @@ module.exports = {
   },
   test: {
     client: 'postgresql',
-    connection: process.env.DATABASE_URL,
+    connection: process.env.TEST_DATABASE_URL,
     migrations: {
       directory: __dirname + "/migrations",
       tableName: 'knex_migrations'
