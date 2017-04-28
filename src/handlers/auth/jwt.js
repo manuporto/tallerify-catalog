94c21e4f344c0f742a5cd4eb0a0a26c7853e8b55
Manuel Porto <manu.porto94@gmail.com>
2017-04-28 11:37:51 -0300

    Db interaction is async
    Use then after query to database

--- src/handlers/auth/jwt.js
+++ src/handlers/auth/jwt.js
@@ -1,20 +1,27 @@
+const logger = require('../../utils/logger');
 const passport = require('passport');
 const db = require('../db/');
 const tables = require('../../database/tableNames');
 const passportJwt = require("passport-jwt");
+const config = require('../../config');
 
 const JwtStrategy = passportJwt.Strategy;
 const extractJwt = passportJwt.ExtractJwt;
 
-const options = {jwtFromRequest: extractJwt.fromAuthHeader(), secretOrKey: 'piensapiensa'};
+const options = {jwtFromRequest: extractJwt.fromAuthHeader(), secretOrKey: config.secret};
 
 passport.use(new JwtStrategy(options, (payload, next) => {
-	const user = db.findWithUsernameAndPassword(tables.users, payload.username, payload.password);
-	if (user) {
-		next(null, user);
-	} else {
-		next(null, false);
-	}
+	logger.info(`Payload: ${JSON.stringify(payload, null, 4)}`);
+	db.findWithUsernameAndPassword(tables.admins, payload.username, payload.password)
+	.then(admin => {
+		if (admin) {
+			logger.info('Success');
+			next(null, admin);
+		} else {
+			logger.info('Failure');
+			next(null, false, { message: 'kb gato' });
+		}
+	});
 }));
 
 module.exports = passport;
