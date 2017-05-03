fc7324f4db169085cfb9af01eede03579d8d2deb
Agustina Barbetta <agustina.barbetta@gmail.com>
2017-05-02 23:28:31 -0300

    Add code to json response

--- src/app.js
+++ src/app.js
@@ -73,11 +73,10 @@ if (app.get('env') === 'development' || app.get('env') === 'test') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use((err, req, res) => {
-  res.status(err.status || 500)
-  .json({
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    code: err.status,
     message: err.message,
-    error: {},
   });
 });
 
