957f9a5c7badf4961e1d47ac821cd398e1d89580
Agustina Barbetta <agustina.barbetta@gmail.com>
2017-05-12 11:28:02 -0300

    Delete unused function

--- src/handlers/db/artistHandler.js
+++ src/handlers/db/artistHandler.js
@@ -2,11 +2,5 @@ const logger = require('../../utils/logger');
 const db = require('../../database/');
 const tables = require('../../database/tableNames');
 
-const selectAllArtistsIdsWithNames = (names) => {
-  logger.info(`Searching for artist ids with names ${names}`);
-  return db(tables.artists).whereIn('name', names).select('id');
-}
 
-module.exports = { 
-	selectAllArtistsIdsWithNames
-};
+module.exports = {};
