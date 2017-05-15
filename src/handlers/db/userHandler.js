5c9987d526367da89e536f24021eeaa1e8475b81
Manuel Porto <manu.porto94@gmail.com>
2017-05-14 23:49:20 -0300

    Add contacts into user's contacts field

--- src/handlers/db/userHandler.js
+++ src/handlers/db/userHandler.js
@@ -12,7 +12,31 @@ const findWithFacebookUserId = (userId) => {
 
 const findAllUsers = () => {
   logger.debug('Getting all users.');
-  return db.from(tables.users).innerJoin(tables.users_users, 'users.id', 'users_users.user_id');
+  return db.raw(`with user_friend as (
+    with friends_ids as (
+    select *
+      from users u
+      left join "users_users" uu on u.id = uu.user_id
+  )
+  select fi.*,  row_to_json(u.*) as contact 
+  from friends_ids fi
+  left join "users" u on fi.friend_id = u.id
+)
+-- select * from user_friend;
+select uf.id, 
+  string_agg(uf."userName", ',') as "userName",
+    string_agg(uf."firstName", ',') as "firstName",
+    string_agg(uf."lastName", ',') as "lastName",
+    string_agg(uf.password, ',') as password,
+    string_agg(uf.country, ',') as country,
+    string_agg(uf.email, ',') as email,
+    string_agg(uf."birthdate", ',') as "birthdate",
+    array_agg(uf.images) as images,
+    array_agg(uf."facebookUserId") as "facebookUserId",
+    string_agg(uf.href, ',') as "href",
+    json_agg(uf.contact) as contacts from user_friend uf
+group by uf.id;`).then((res) => res.rows);
+  //return db.from(tables.users).innerJoin(tables.users_users, 'users.id', 'users_users.user_id');
 };
 
 const friend = (userId, friendId) => {
