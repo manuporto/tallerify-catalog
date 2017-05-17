8dca87342e9eaf9e25a19b68cd25f3d6651f888f
Agustina Barbetta <agustina.barbetta@gmail.com>
2017-05-17 01:23:39 -0300

    Add album tables

--- src/database/migrations/migrations.js
+++ src/database/migrations/migrations.js
@@ -9,7 +9,7 @@ exports.up = (knex, Promise) => {
 			table.increments('id').primary();
 			table.string('name');
 			table.integer('duration');
-      table.integer('albumId');
+      table.integer('album_id');
       table.string('href');
 		}),
 
@@ -27,6 +27,22 @@ exports.up = (knex, Promise) => {
 			table.string('name');
 			table.integer('popularity');
 		}),
+
+    knex.schema.createTableIfNotExists(tables.artists, (table) => {
+      logger.debug('Creating albums table.');
+      table.increments('id').primary();
+      table.string('name');
+      table.integer('release_date');
+      table.specificType('images', 'text ARRAY');
+      table.specificType('genres', 'text ARRAY');
+    }),
+
+    knex.schema.createTableIfNotExists(tables.albums_artists, (table) => {
+      logger.debug('Creating albums_artists table.');
+      table.increments('album_artist_id').primary();
+      table.integer('album_id');
+      table.integer('artist_id');
+    }),
 
 		knex.schema.createTableIfNotExists(tables.artists_tracks, (table) => {
 			logger.debug('Creating artists_tracks table.');
@@ -74,6 +90,7 @@ exports.up = (knex, Promise) => {
 exports.down = (knex, Promise) => {
 	return Promise.all([
     knex.schema.dropTable(tables.tracks),
+    knex.schema.dropTable(tables.albums),
     knex.schema.dropTable(tables.tracks_rating),
     knex.schema.dropTable(tables.artists),
     knex.schema.dropTable(tables.artists_tracks),
