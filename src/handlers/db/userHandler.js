e5a6605bedafa8709da5bcb457d245aa4e613788
Manuel Porto <manu.porto94@gmail.com>
2017-06-17 18:48:52 -0300

    Typo

--- src/handlers/db/userHandler.js
+++ src/handlers/db/userHandler.js
@@ -20,7 +20,7 @@ const _findAllUsers = () => db
 
 const findAllUsers = queries => {
   logger.debug('Getting all users.');
-  return queries.name ? _findAllUsers().where('u.name', queries.name) : _findAllUsers();
+  return queries.name ? _findAllUsers().where('u.userName', queries.name) : _findAllUsers();
 };
 
 const findUser = id => {
